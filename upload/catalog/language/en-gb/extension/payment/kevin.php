d26700450ba5fcce2ce6c57c2d17a85a990705c1
Agnet-group <73598849+Agnet-group@users.noreply.github.com>
2022-06-22 11:53:17 +0300

    updated errors and alerts

--- upload/catalog/language/en-gb/extension/payment/kevin.php
+++ upload/catalog/language/en-gb/extension/payment/kevin.php
@@ -15,31 +15,36 @@
 *  @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
 */
 // Text
-$_['text_title'] = 'kevin.';
-$_['text_order_info'] = 'Order ID %s';
-$_['text_card_name'] = 'Credit/Debit Card';
-$_['text_kevin_payment_method'] = 'Payment Method: %s';
-$_['text_status'] = 'Order Status: %s';
-$_['text_status_group'] = 'Order Status Group: %s';
-$_['text_refund_status'] = 'Refund Status: %s';
-$_['text_payment_id'] = 'Payment ID: %s';
-$_['text_loading'] = 'Loading';
-$_['text_select_country'] = '--Select Country--';
-$_['text_countries'] = 'Countries';
-$_['text_amount_received'] = 'Amount refunded: %s';
-$_['text_reason'] = 'Refunded on: %s';
-$_['text_subject'] = '%s has refunded your money. Order ID: %s';
-$_['text_thank_you'] = 'Thank You for buying <br /> <strong>%s</strong>';
+$_['text_title']                           = 'kevin.';
+$_['text_order_info']                      = 'Order ID %s';
+$_['text_card_name']                       = 'Credit/Debit Card';
+$_['text_kevin_payment_method']            = 'Payment Method: %s';
+$_['text_status']                          = 'Order Status: %s';
+$_['text_status_group']                    = 'Order Status Group: %s';
+$_['text_refund_status']                   = 'Refund Status: %s';
+$_['text_payment_id']                      = 'Payment ID: %s';
+$_['text_loading']                         = 'Loading';
+$_['text_select_country']                  = '--Select Country--';
+$_['text_countries']                       = 'Countries';
+$_['text_amount_received']                 = 'Amount refunded: %s';
+$_['text_reason']                          = 'Refunded on: %s';
+$_['text_subject']                         = '%s has refunded your money. Order ID: %s';
+$_['text_thank_you']                       = 'Thank You for buying <br /> <strong>%s</strong>';
+$_['text_sandbox_alert']                   = 'This payment method is set to Sandbox mode. Only for test payments. Live payments is not available!';
 
-// errors
-$_['error_currency'] = 'Only &euro; currency are allowed for this payment method. Please select a currency <strong>&euro;</strong>.';
-$_['error_bank'] = 'Bank not selected! Choose your bank.';
-$_['error_kevin_payment'] = 'An error occurred. Please contact the merchant for more information, or try another payment method.';
-$_['error_kevin_payment_id'] = 'An error occurred. Payment ID was not returned from the payment system. Please contact the merchant for more information.';
+//errors
+$_['error_currency']                       = 'Only &euro; currency are allowed for this payment method. Please select a currency <strong>&euro;</strong>.';
+$_['error_bank']                           = 'Bank not selected! Choose your bank.';
+$_['error_kevin_payment']                  = 'An error occurred. Please contact the merchant for more information, or try another payment method.';
+$_['error_kevin_payment_id']               = 'An error occurred. Payment ID was not returned from the payment system. Please contact the merchant for more information.';
+$_['error_missing_data']                   = 'Data for payment is missing! Please try again, or choose another payment method.';
+$_['error_payment_option']                 = 'No payment options using this payment method available.';
+$_['error_order_session']                  = 'An error occurred. Order Session have been ended! Please try again.';
+$_['error_response']                       = 'An error occurred! On response not received any statusGroup. Description: Server Error. Please try again.';
 
-// Button
-$_['button_confirm'] = 'Confirm Order';
-$_['button_search'] = 'Search Autocomplete';
+//Button
+$_['button_confirm']                       = 'Confirm Order';
+$_['button_search']                        = 'Search Autocomplete';
 
-// Help
-$_['help_serch_bank'] = 'Search Bank';
+//Help
+$_['help_serch_bank']                      = 'Search Bank';
