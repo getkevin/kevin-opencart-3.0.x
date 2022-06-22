3239893587e6bfda9048146173aff7c34807f765
Agnet-group <73598849+Agnet-group@users.noreply.github.com>
2022-06-22 11:58:17 +0300

    updated errors and alerts

--- upload/catalog/language/es-es/extension/payment/kevin.php
+++ upload/catalog/language/es-es/extension/payment/kevin.php
@@ -15,31 +15,35 @@
 *  @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
 */
 // Text
-$_['text_amount_received'] = 'Importe devuelto: %s';
-$_['text_card_name'] = 'Tarjeta de crédito / débito';
-$_['text_countries'] = 'Paises';
-$_['text_kevin_payment_method'] = 'Forma de pago: %s';
-$_['text_loading'] = 'Cargar';
-$_['text_order_info'] = 'ID de pedido %s';
-$_['text_payment_id'] = 'ID de pago: %s';
-$_['text_reason'] = 'Reembolsado: %s';
-$_['text_refund_status'] = 'Estado del reembolso: %s';
-$_['text_select_country'] = '-- Seleccione un país--';
-$_['text_status'] = 'Estado del pedido: %s';
-$_['text_status_group'] = 'Grupo de estado de pedido: %s';
-$_['text_subject'] = '%s le ha reembolsado su dinero. ID de pedido: %s';
-$_['text_thank_you'] = 'Gracias por comprar <br /> <strong> %s </strong>';
-$_['text_title'] = 'kevin.';
+$_['text_amount_received']      = 'Importe devuelto: % s';
+$_['text_card_name']            = 'Tarjeta de crédito / débito';
+$_['text_countries']            = 'Paises';
+$_['text_kevin_payment_method'] = 'Forma de pago: % s';
+$_['text_loading']              = 'Cargar';
+$_['text_order_info']           = 'ID de pedido %s';
+$_['text_payment_id']           = 'ID de pago: % s';
+$_['text_reason']               = 'Reembolsado: % s';
+$_['text_refund_status']        = 'Estado del reembolso: % s';
+$_['text_select_country']       = '-- Seleccione un país--';
+$_['text_status']               = 'Estado del pedido: % s';
+$_['text_status_group']         = 'Grupo de estado de pedido: % s';
+$_['text_subject']              = '%s le ha reembolsado su dinero. ID de pedido: %s';
+$_['text_thank_you']            = 'Gracias por comprar <br /> <strong> %s </strong>';
+$_['text_title']                = 'kevin.';
+$_['text_sandbox_alert']        = 'Este método de pago está configurado en modo Sandbox. Solo para pagos de prueba. ¡Los pagos reales no están disponibles!';
 
-// errors
-$_['error_bank'] = 'Banco no seleccionado! Elige tu banco.';
-$_['error_currency'] = 'Seleccione <strong> & euro; </strong > como moneda de pago.';
-$_['error_kevin_payment'] = 'Error. Ponte en contacto con el vendedor para obtener más información o prueba a utilizar otro método de pago.';
-$_['error_kevin_payment_id'] = 'Error. El sistema de pago no devuelve el ID de pago. Ponte en contacto con el vendedor para obtener más información.';
+// Error
+$_['error_currency']            = 'Seleccione <strong> & euro; </strong > como moneda de pago.';
+$_['error_bank']                = 'Banco no seleccionado! Elige tu banco.';
+$_['error_kevin_payment']       = 'Error. Ponte en contacto con el vendedor para obtener más información o prueba a utilizar otro método de pago.';
+$_['error_kevin_payment_id']    = 'Error. El sistema de pago no devuelve el ID de pago. Ponte en contacto con el vendedor para obtener más información.';
+$_['error_missing_data']        = 'Sin detalles de pago! Vuelve a intentarlo o elige otro método de pago.';
+$_['error_order_session']       = 'Hubo un error. ¡Sesión completada! Inténtalo de nuevo.';
+$_['error_payment_option']      = 'El pago con este método de pago no está disponible.';
 
 // Button
-$_['button_confirm'] = 'Confirmar el pedido';
-$_['button_search'] = 'Autocompletar de búsqueda';
+$_['button_confirm']            = 'Confirmar el pedido';
+$_['button_search']             = 'Autocompletar de búsqueda';
 
 // Help
-$_['help_serch_bank'] = 'Encontrar un banco';
+$_['help_serch_bank']           = 'Encontrar un banco';
