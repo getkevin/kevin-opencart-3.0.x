1838a4d5bfd6fd6509f6cb5ddf21fb33322a2f7b
Agnet-group <73598849+Agnet-group@users.noreply.github.com>
2022-06-22 12:05:32 +0300

    updated errors and alerts

--- upload/catalog/language/lt-lt/extension/payment/kevin.php
+++ upload/catalog/language/lt-lt/extension/payment/kevin.php
@@ -15,31 +15,35 @@
 *  @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
 */
 // Text
-$_['text_title'] = 'kevin.';
-$_['text_order_info'] = 'Užsakymo ID %s';
-$_['text_card_name'] = 'Keredito/Debeto kortelė';
+$_['text_amount_received']      = 'Grąžinta suma: %s';
+$_['text_card_name']            = 'Keredito/Debeto kortelė';
+$_['text_countries']            = 'Šalys';
 $_['text_kevin_payment_method'] = 'Mokėjimo būdas: %s';
-$_['text_status'] = 'Užsakymo statusas: %s';
-$_['text_status_group'] = 'Užsakymo satuso grupė: %s';
-$_['text_refund_status'] = 'Grąžinimo statusas: %s';
-$_['text_payment_id'] = 'Mokėjimo ID: %s';
-$_['text_loading'] = 'Kraunasi';
-$_['text_select_country'] = '--Pasirinkite šalį--';
-$_['text_countries'] = 'Šalys';
-$_['text_amount_received'] = 'Grąžinta suma: %s';
-$_['text_reason'] = 'Grąžinta, kad: %s';
-$_['text_subject'] = '%s grąžino jūsų pinigus. Užsakymo ID: %s';
-$_['text_thank_you'] = 'Ačiū kad perkate <br /> <strong>%s</strong>';
+$_['text_loading']              = 'Kraunasi';
+$_['text_order_info']           = 'Užsakymo ID %s';
+$_['text_payment_id']           = 'Mokėjimo ID: %s';
+$_['text_reason']               = 'Grąžinta: %s';
+$_['text_refund_status']        = 'Grąžinimo statusas: %s';
+$_['text_select_country']       = '--Pasirinkite šalį--';
+$_['text_status']               = 'Užsakymo statusas: %s';
+$_['text_status_group']         = 'Užsakymo satuso grupė: %s';
+$_['text_subject']              = '%s grąžino jūsų pinigus. Užsakymo ID: %s';
+$_['text_thank_you']            = 'Ačiū kad perkate <br /> <strong>%s</strong>';
+$_['text_title']                = 'kevin.';
+$_['text_sandbox_alert']        = 'Šis mokėjimo būdas nustatytas į "sandbox" režimą. Tik bandomiesiems mokėjimams. Tiesioginiai mokėjimai neprieinami!';
 
-// errors
-$_['error_currency'] = 'Jei norite atsiskaityti šiuo mokėjimo būdu, pasirinkite valiutą  <strong>&euro;</strong>.';
-$_['error_bank'] = 'Bankas nepasirinktas! Pasirinkite savo banką.';
-$_['error_kevin_payment'] = 'Įvyko klaida. Norėdami gauti daugiau informacijos, susisiekite su pardavėju arba bandykite atsiskaityti naudodami  kitą mokėjimo metodą.';
-$_['error_kevin_payment_id'] = 'Įvyko klaida. Iš mokėjimo sistemos negrąžintas mokėjimo ID. Norėdami gauti daugiau informacijos, susisiekite su pardavėju.';
+// Error
+$_['error_missing_data']        = 'Trūksta mokėjimo duomenų! Bandykite dar kartą arba pasirinkite kitą mokėjimo būdą.';
+$_['error_order_session']       = 'Įvyko klaida. Užsakymo sesija baigėsi! Pabandykite dar kartą.';
+$_['error_payment_option']      = 'Mokėjimas naudojant šį mokėjimo būdą negalimas.';
+$_['error_bank']                = 'Bankas nepasirinktas! Pasirinkite savo banką.';
+$_['error_currency']            = 'Jei norite atsiskaityti šiuo mokėjimo būdu, pasirinkite valiutą <strong>&euro;</strong>.';
+$_['error_kevin_payment']       = 'Įvyko klaida. Norėdami gauti daugiau informacijos, susisiekite su pardavėju arba bandykite atsiskaityti naudodami kitą mokėjimo metodą.';
+$_['error_kevin_payment_id']    = 'Įvyko klaida. Iš mokėjimo sistemos negrąžintas mokėjimo ID. Norėdami gauti daugiau informacijos, susisiekite su pardavėju.';
 
 // Button
-$_['button_confirm'] = 'Patvirtinti užsakymą';
-$_['button_search'] = 'Paieškos automatinis užbaigimas';
+$_['button_confirm']            = 'Patvirtinti užsakymą';
+$_['button_search']             = 'Paieškos automatinis užbaigimas';
 
 // Help
-$_['help_serch_bank'] = 'Ieškoti banką';
+$_['help_serch_bank']           = 'Ieškoti banką';
