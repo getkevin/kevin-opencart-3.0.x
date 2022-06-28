e9fc19c9e392fbb5609bcaeb28f4f9c3ed926b9e
Šaltanovas Aivaras <aivaras.saltanovas@kevin.eu>
2022-06-28 11:13:44 +0300

    Translations and more

--- upload/admin/language/en-gb/extension/module/kevin_refund.php
+++ upload/admin/language/en-gb/extension/module/kevin_refund.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * 2020 kevin. payment  for OpenCart version 3.0.x.x
-* @version 1.0.1.4
+* @version 1.0.1.5
 *
 * NOTICE OF LICENSE
 *
@@ -19,96 +19,100 @@
 $_['heading_title'] = '<span style="font-weight: 700; color:red;">kevin. </span> Refunds';
 
 // Text
+$_['text_comment'] = 'Customer Comment';
+$_['text_confirm_refund'] = 'Are you sure you want to refund this order?';
+$_['text_create_refund'] = 'Create Refund';
+$_['text_customer'] = 'Customer';
+$_['text_customer_detail'] = 'Customer Details';
+$_['text_customer_group'] = 'Customer Group';
+$_['text_date_added'] = 'Date Added';
+$_['text_default'] = 'Default';
+$_['text_ean'] = 'EAN:';
+$_['text_email'] = 'E-Mail';
+$_['text_fax'] = 'Fax';
+$_['text_firstname'] = 'First Name';
+$_['text_isbn'] = 'ISBN:';
+$_['text_jan'] = 'JAN:';
+$_['text_lastname'] = 'Last Name';
+$_['text_list'] = 'Order List';
+$_['text_missing'] = 'Missing Orders';
+$_['text_mpn'] = 'MPN:';
+$_['text_order'] = 'Order (#%s)';
+$_['text_order_detail'] = 'Order Details';
+$_['text_order_id'] = 'Order ID:';
+$_['text_payment_method'] = 'Payment Method';
+$_['text_product_restock'] = 'Restock:';
+$_['text_Qty'] = 'Qty:';
+$_['text_reason'] = 'Reason for refund (optional)';
+$_['text_refund'] = 'Refund';
+$_['text_refunded'] = 'Amount already refunded Total:';
+$_['text_refund_amount'] = 'Enter amount for refund';
+$_['text_refund_available'] = 'Amount available for refund';
+$_['text_refund_empty'] = 'Refund amount field can not be empty! Please enter amount for refund.';
+$_['text_refund_history'] = 'Refund History';
+$_['text_refund_order_id'] = '<span style="font-weight: 700; color:red;">kevin.</span> Refund... Order ID:';
+$_['text_restock'] = 'Success: Products have been restocked!';
+$_['text_restocked_products'] = 'Restocked Products';
+$_['text_shipping_method'] = 'Shipping Method';
+$_['text_sku'] = 'SKU:';
 $_['text_store'] = 'Store';
 $_['text_success'] = 'Success: You have modified <span style="font-weight: 700; color:red;">kevin. </span> Refunds!';
-$_['text_list'] = 'Order List';
-$_['text_order_detail'] = 'Order Details';
-$_['text_customer_detail'] = 'Customer Details';
-$_['text_refund_history'] = 'Refund History';
-$_['text_refund_available'] = 'Amount available for refund';
-$_['text_refunded'] = 'Amount already refunded Total:';
-$_['text_refund_order_id'] = '<span style="font-weight: 700; color:red;">kevin.</span> Refund... Order ID:';
-$_['text_product_restock'] = 'Restock: ';
-$_['text_Qty'] = 'Qty: ';
-$_['text_refund_amount'] = 'Enter amount for refund';
-$_['text_reason'] = 'Reason for refund (optional)';
-$_['text_refund'] = 'Refund ';
-$_['text_create_refund'] = 'Create Refund ';
-$_['text_confirm_refund'] = 'Are you sure you want to refund this order?';
-$_['text_refund_empty'] = 'Refund amount field can not be empty! Please enter amount for refund.';
-$_['text_date_added'] = 'Date Added';
-$_['text_payment_method'] = 'Payment Method';
-$_['text_shipping_method'] = 'Shipping Method';
-$_['text_customer'] = 'Customer';
-$_['text_customer_group'] = 'Customer Group';
-$_['text_firstname'] = 'First Name';
-$_['text_lastname'] = 'Last Name';
-$_['text_email'] = 'E-Mail';
 $_['text_telephone'] = 'Telephone';
-$_['text_fax'] = 'Fax';
-$_['text_comment'] = 'Customer Comment';
-$_['text_order'] = 'Order (#%s)';
-$_['text_order_id'] = 'Order ID:';
-$_['text_sku'] = 'SKU:';
 $_['text_upc'] = 'UPC:';
-$_['text_ean'] = 'EAN:';
-$_['text_jan'] = 'JAN:';
-$_['text_isbn'] = 'ISBN:';
-$_['text_mpn'] = 'MPN:';
-$_['text_missing'] = 'Missing Orders';
-$_['text_default'] = 'Default';
-$_['text_restock'] = 'Success: Products have been restocked!';
 $_['text_upload'] = 'Your file was successfully uploaded!';
-$_['text_restocked_products'] = 'Restocked Products';
+$_['text_sandbox_alert'] = '<span style="font-weight: 600; color:red;">kevin.</span> payment gateway is set to sandbox mode. For testing purposes only. Actual payments not available!';
+$_['text_refund_warning'] = '<span style="font-weight: 600; color:red;">kevin.</span> refunds are not allowed! Please contact <a style="font-weight: 800; color:red;" href="https://kevin.eu" target="_blank">kevin.</a>';
+$_['text_refund_prepare'] = 'Amount prepared for refund: %1$s. Refund status: "%2$s". Order No. %3$s.';
 
 // Button
-$_['button_refund'] = 'Create Refund';
-$_['button_edit'] = 'Edit';
 $_['button_cancel'] = 'Cancel';
+$_['button_edit'] = 'Edit';
+$_['button_refund'] = 'Create Refund';
 
 // Column
-$_['column_refund_id'] = 'kevin.\'s Refund ID';
-$_['column_order_id'] = 'Order ID';
-$_['column_customer'] = 'Customer';
-$_['column_status'] = 'Status';
-$_['column_refund_status'] = 'Refund Status';
+$_['column_action'] = 'Action';
 $_['column_amount'] = 'Refunded';
+$_['column_amount_available'] = 'Amount Available for refund';
 $_['column_amount_ready'] = 'Ready to Refund';
-$_['column_refunded_amount'] = 'Amount Refunded';
-$_['column_reason'] = 'Reason for refund';
-$_['column_amount_available'] = 'Amaunt Available for refund';
-$_['column_refund_action'] = 'Refund Action';
+$_['column_customer'] = 'Customer';
 $_['column_date_added'] = 'Date Added';
 $_['column_date_modified'] = 'Date Modified';
-$_['column_total'] = 'Total';
-$_['column_product'] = 'Product';
-$_['column_model'] = 'Model';
-$_['column_quantity'] = 'Quantity';
-$_['column_restocked_quantity'] = 'Quantity Restocked';
-$_['column_price'] = 'Unit Price (stock)';
-// $_['column_comment']             = 'Comment';
-$_['column_action'] = 'Action';
-// $_['column_weight']              = 'Product Weight';
 $_['column_image'] = 'Image';
+$_['column_model'] = 'Model';
 $_['column_name'] = 'Product name';
+$_['column_order_id'] = 'Order ID';
+$_['column_price'] = 'Unit Price (stock)';
+$_['column_product'] = 'Product';
+$_['column_quantity'] = 'Quantity';
+$_['column_reason'] = 'Reason for refund';
+$_['column_refunded_amount'] = 'Amount Refunded';
+$_['column_refund_action'] = 'Refund Action';
+$_['column_refund_id'] = 'kevin.\'s Refund ID';
+$_['column_refund_status'] = 'Refund Status';
+$_['column_restocked_quantity'] = 'Quantity Restocked';
+$_['column_status'] = 'Status';
+$_['column_total'] = 'Total';
 
 // Entry
-$_['entry_store'] = 'Store';
-$_['entry_customer'] = 'Customer';
-$_['entry_customer_group'] = 'Customer Group';
-$_['entry_firstname'] = 'First Name';
-$_['entry_lastname'] = 'Last Name';
-$_['entry_order_status'] = 'Order Status';
-$_['entry_notify'] = 'Notify Customer';
 $_['entry_comment'] = 'Comment';
 $_['entry_currency'] = 'Currency';
-$_['entry_order_id'] = 'Order ID';
-$_['entry_total'] = 'Total';
+$_['entry_customer'] = 'Customer';
+$_['entry_customer_group'] = 'Customer Group';
 $_['entry_date_added'] = 'Date Added';
 $_['entry_date_modified'] = 'Date Modified';
+$_['entry_firstname'] = 'First Name';
+$_['entry_lastname'] = 'Last Name';
+$_['entry_notify'] = 'Notify Customer';
+$_['entry_order_id'] = 'Order ID';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_store'] = 'Store';
+$_['entry_total'] = 'Total';
 
 // Error
-$_['error_warning'] = 'Warning: Please check the form carefully for errors!';
-$_['error_permission'] = 'Warning: You do not have permission to modify kevin. Refunds!';
 $_['error_action'] = 'Warning: Could not complete this action!';
 $_['error_filetype'] = 'Invalid file type!';
+$_['error_permission'] = 'Warning: You do not have permission to modify kevin. Refunds!';
+$_['error_warning'] = 'Warning: Please check the form carefully for errors!';
+$_['error_auth_refund'] = 'An error occurred! You are not authorized to make refunds. Please contact <a style="font-weight: 800; color:red;" href="https://kevin.eu" target="_blank">kevin.</a>';
+$_['error_amount_refund'] = 'An error occurred! Refund amount was not entered. Please try again by entering the Refund Amount.';
+$_['error_field_refund'] = 'An error occurred! Please check the form, fill in all required fields and try again.';
--- upload/catalog/controller/extension/payment/kevin.php
+++ upload/catalog/controller/extension/payment/kevin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * 2020 kevin. payment  for OpenCart version 3.0.x.x
-* @version 1.0.1.4
+* @version 1.0.1.5
 *
 * NOTICE OF LICENSE
 *
@@ -21,7 +21,7 @@ class ControllerExtensionPaymentKevin extends Controller
     private $type = 'payment';
     private $name = 'kevin';
     private $lib_version = '0.3';
-    private $plugin_version = '1.0.1.4';
+    private $plugin_version = '1.0.1.5';
 
     public function index()
     {
@@ -47,7 +47,6 @@ class ControllerExtensionPaymentKevin extends Controller
         $this->load->model('extension/payment/kevin');
         $this->load->language('extension/payment/kevin');
         $this->load->model('localisation/language');
-
         $current_language = $this->config->get('config_language_id');
 
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
@@ -95,7 +94,7 @@ class ControllerExtensionPaymentKevin extends Controller
         $banks = $kevinClient->auth()->getBanks($country_code);
 
         $project_settings = $kevinClient->auth()->getProjectSettings();
-        //	echo '<pre>'; print_r($project_settings); echo '</pre>';
+
         $payment_methods = $project_settings['paymentMethods'];
 
         // card first
@@ -122,10 +121,12 @@ class ControllerExtensionPaymentKevin extends Controller
 
         $data['text_sandbox_alert'] = '';
         if ($project_settings['isSandbox']) {
-            $data['text_sandbox_alert'] = 'This payment method is set to Sandbox mode. Only for test payments. Real payments is not available!';
+            $data['text_sandbox_alert'] = $this->language->get('text_sandbox_alert');
         }
 
-        if (file_exists(DIR_APPLICATION.'controller/extension/payment/kevin_image/credit_card_icon.png')) {
+        if (@getimagesize('https://cdn.kevin.eu/banks/images/VISA_MC.png')) {
+            $data['credit_card_icon'] = 'https://cdn.kevin.eu/banks/images/VISA_MC.png';
+        } elseif (file_exists(DIR_APPLICATION.'controller/extension/payment/kevin_image/credit_card_icon.png')) {
             $data['credit_card_icon'] = $this->config->get('config_url').'catalog/controller/extension/payment/kevin_image/credit_card_icon.png';
         } else {
             $data['credit_card_icon'] = '';
@@ -170,8 +171,8 @@ class ControllerExtensionPaymentKevin extends Controller
         } else {
             $bank_id = '';
             $payment_method = '';
-            $this->session->data['error'] = 'Data for payment is missing! Please try again, or choose another payment method.';
-            $this->KevinLog($this->session->data['error']);
+            $this->session->data['error'] = $this->language->get('error_missing_data');
+            $this->KevinLog('Data for payment is missing! Please try again, or choose another payment method.');
             $this->response->redirect($this->url->link('checkout/cart', '', true));
         }
 
@@ -304,13 +305,12 @@ class ControllerExtensionPaymentKevin extends Controller
         } else {
             $log_data = 'Answer on Redirect kevin... No payment options using this payment method available.';
             $this->KevinLog($log_data);
-            $this->session->data['error'] = 'No payment options using this payment method available.';
+            $this->session->data['error'] = $this->language->get('error_payment_option');
             $this->response->redirect($this->url->link('checkout/cart', '', true));
         }
 
         $init_payment = $kevinClient->payment()->initPayment($payment_attr);
 
-        //	echo '<pre>response:';	print_r($init_payment); echo '</pre>'; die;
         if (!empty($init_payment['id'])) {
             $payment_id = $init_payment['id'];
         } else {
@@ -345,22 +345,19 @@ class ControllerExtensionPaymentKevin extends Controller
         /* log */
         $log_data = 'Answer on Redirect kevin... Payment Method: '.$payment_method.'; Payment ID: '.$payment_id.'; Order ID: '.$order_id.'; Payment Status: '.$get_payment['statusGroup'].'; Total: '.$get_payment['amount'].$get_payment['currencyCode'].'; Bank ID: '.$bank_id.'.';
         $this->KevinLog($log_data);
-        /*
-        if (!empty($order_info['payment_iso_code_2'])) {
-            $current_country_code = $order_info['payment_iso_code_2'];
-        } else if (isset($this->session->data['iso_code_2'])) {
-            $current_country_code = $this->session->data['iso_code_2'];
-        }
-        */
+
         $lang_code = $this->language->get('code');
-        $available_lang = ['en', 'lt', 'lv', 'ee', 'fi', 'se', 'ru'];
+        /*
+        $available_lang = array('cs', 'en', 'es', 'et', 'fi', 'it', 'lt', 'lv', 'pt', 'ru', 'sk', 'sv');
         if (in_array($lang_code, $available_lang)) {
             $lang = $lang_code;
         } else {
             $lang = 'en';
         }
+        */
+        $lang = $lang_code;
         unset($this->session->data['order_id']);
-        // header('Location:' . $init_payment['confirmLink'] . '&amp;lang=' . $lang);
+
         $this->response->redirect($init_payment['confirmLink'].'&amp;lang='.$lang);
     }
 
@@ -406,14 +403,18 @@ class ControllerExtensionPaymentKevin extends Controller
         } else {
             $order_id = 0;
             $payment_method = '';
-            $this->session->data['error'] = 'An error occurred. Order Session have been ended! Please try again.';
-            $this->KevinLog($this->session->data['error']);
+            $this->session->data['error'] = $this->language->get('error_order_session');
+            $this->KevinLog('An error occurred. Order Session have been ended!');
             $this->response->redirect($this->url->link('checkout/cart', '', true));
         }
 
         $order_info = $this->model_checkout_order->getOrder($order_id);
 
         switch ($statusGroup) {
+            case 'started':
+                $new_status_id = $this->config->get('payment_kevin_started_status_id');
+                $new_status = $statusGroup;
+                break;
             case 'pending':
                 $new_status_id = $this->config->get('payment_kevin_pending_status_id');
                 $new_status = $statusGroup;
@@ -432,13 +433,14 @@ class ControllerExtensionPaymentKevin extends Controller
         }
 
         if (!$new_status_id) {
-            $this->session->data['error'] = 'An error occurred! On response not received any statusGroup. Description: Server Error. Please try again.';
-            $this->KevinLog($this->session->data['error']);
+            $this->session->data['error'] = $this->language->get('error_response');
+            $this->KevinLog('An error occurred! On response not received any statusGroup. Description: Server Error.');
             $this->response->redirect($this->url->link('checkout/cart', '', true));
         }
 
         /* log */
         $log_data = 'Answer on Confirm kevin... Payment Method: '.$payment_method.'; Payment ID: '.$payment_id.'; Order ID: '.$order_info['order_id'].'; Payment Status: '.$new_status.'.';
+        $this->KevinLog($log_data);
 
         if ($new_status == 'completed') {
             $this->response->redirect($this->url->link('checkout/success', '', true));
@@ -446,6 +448,8 @@ class ControllerExtensionPaymentKevin extends Controller
             $this->response->redirect($this->url->link('checkout/success', '', true));
         } elseif ($new_status == 'failed') {
             $this->response->redirect($this->url->link('checkout/failure', '', true));
+        } elseif ($new_status == 'started') {
+            $this->response->redirect($this->url->link('checkout/cart', '', true));
         } else {
             $this->session->data['error'] = $this->language->get('error_kevin_payment');
             $this->response->redirect($this->url->link('checkout/cart'));
@@ -581,8 +585,7 @@ class ControllerExtensionPaymentKevin extends Controller
         $order_info = $this->model_checkout_order->getOrder($order_id);
 
         $log_data = 'Answer on WebHook kevin... Payment Method: '.$payment_method.'; Payment ID: '.$payment_id.'; Order ID: '.$order_info['order_id'].'; Payment Status: '.$new_status.'.';
-
-        // $old_status_id = $order_info['order_status_id'];
+        $this->KevinLog($log_data);
 
         if ($payment_method == 'card') {
             $update_order['status'] = !empty($get_payment_status['hybridStatus']) ? $get_payment_status['hybridStatus'] : $get_payment_status['cardStatus'];
@@ -898,7 +901,6 @@ class ControllerExtensionPaymentKevin extends Controller
         $message .= '  <body>';
         $message .= $logo_view.'<br /><br /><br />';
         $message .= html_entity_decode($body, \ENT_QUOTES, 'UTF-8');
-        // $message .= $body;
         $message .= '</body>'."\n";
         $message .= '</html>'."\n";
 
